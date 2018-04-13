33dc9e1caa10e889459ad4bbb5106e1d68e24193
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2018-04-13 15:07:26 -0400

    Somy styling done. Javascript calls done. Scroll function done.

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://hacker-news.firebaseio.com/v0/topstories.json');
-  xhr.send('topstories');
-  xhr.onreadystatechange = function () {
-    var DONE = 4; // readyState 4 means the request is done.
-    var OK = 200; // status 200 is a successful return.
-    if (xhr.readyState === DONE) {
-      console.log(xhr.status);
-      if (xhr.status === 200)   {
-        console.log(xhr.responseText); // 'This is the returned text.'
-      } else {
-        console.log('Error: ' + xhr.status); // An error occurred during the request.
-      }
+  let array = []
+  let min = 0;
+  let max = 30;
+  fetch('https://hacker-news.firebaseio.com/v0/topstories.json').then(function(result) {
+    return result.json();
+  }).then(function(storyIds) {
+    array = storyIds;
+    for (let i = min;  i < max; i++) {
+      fetch(`https://hacker-news.firebaseio.com/v0/item/${storyIds[i]}.json`).then(function(result) {
+        return result.json();
+      }).then(function(story) {
+        var node = document.createElement("LI");
+        let text =  document.createTextNode(`${story.title}`);
+        node.appendChild(text);
+        document.getElementById('stories').appendChild(node);
+      })
     }
-  };
+    min = max;
+    max += 30;
+ })
+  document.onscroll = function() {
+    if(document.documentElement.scrollTop + window.innerHeight == document.documentElement.scrollHeight)
+    {
+      for (let i = min;  i < max; i++) {
+        fetch(`https://hacker-news.firebaseio.com/v0/item/${array[i]}.json`).then(function(result) {
+        return result.json();
+        }).then(function(story) {
+          var node = document.createElement("LI");
+          let text =  document.createTextNode(`${story.title}`);
+          node.appendChild(text);
+          document.getElementById('stories').appendChild(node);
+        })
+      }
+      min = max;
+      max += 30;
+    }
+  }
 });
