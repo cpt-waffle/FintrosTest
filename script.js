7a10d0d859fbc901c1c2d8cfaf3790cf3a674ed1
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2018-04-13 17:40:26 -0400

    Added hours ago.

--- script.js
+++ script.js
@@ -10,9 +10,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
       fetch(`https://hacker-news.firebaseio.com/v0/item/${storyIds[i]}.json`).then(function(result) {
         return result.json();
       }).then(function(story) {
+        var linkNode = document.createElement("A");
+        linkNode.setAttribute("href", story.url)
         var node = document.createElement("LI");
         let text =  document.createTextNode(`${story.title}`);
+
+        let then = new Date(story.time*1000);
+        let now = new Date();
+        let hours = Math.floor((now - then) / 1000 / 60 / 60);
+        var bottomData =  document.createTextNode(`${story.score} points by ${story.by} ${hours} hours ago`);
+        let bottomDataNode = document.createElement('P');
+        bottomDataNode.className = "stats-info";
+
+        bottomDataNode.appendChild(bottomData);
+
         node.appendChild(text);
+        linkNode.appendChild(text);
+        node.appendChild(linkNode);
+        node.appendChild(bottomDataNode);
         document.getElementById('stories').appendChild(node);
       })
     }
@@ -26,9 +41,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
         fetch(`https://hacker-news.firebaseio.com/v0/item/${array[i]}.json`).then(function(result) {
         return result.json();
         }).then(function(story) {
+          var linkNode = document.createElement("A");
+          linkNode.setAttribute("href", story.url);
+
           var node = document.createElement("LI");
           let text =  document.createTextNode(`${story.title}`);
+
+          var bottomData =  document.createTextNode(`${story.score} points by ${story.by} ${ new Date(story.time).toDateString()} hours ago`);
+          let bottomDataNode = document.createElement('P');
+          bottomDataNode.className = "stats-info";
+
+          bottomDataNode.appendChild(bottomData);
+
           node.appendChild(text);
+          linkNode.appendChild(text);
+          node.appendChild(linkNode);
+          node.appendChild(bottomDataNode);
           document.getElementById('stories').appendChild(node);
         })
       }
